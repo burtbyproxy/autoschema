4fc070d31bc22a8da862848d0e50c1a92f185d59
Steve Elliott <steve@datadigital.net>
2025-02-20 10:04:40 -0800

    Setup initial config files.

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import importPlugin from "eslint-plugin-import";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all
+});
+
+export default [
+	...compat.extends("eslint:recommended"),
+	importPlugin.flatConfigs.recommended,
+	{
+		languageOptions: {
+			globals: {
+				...globals.node,
+				...globals.browser
+			}
+		},
+		rules: {
+			// "no-unused-vars": "off",
+			"import/order": [1, { groups: ["external", "builtin", "internal", "sibling", "parent", "index"] }]
+		}
+	},
+	{
+		ignores: ["node_modules/*", "dist/*", "build/*", ".cache/*", "*.min.js", "*.bundle.js"]
+	},
+	eslintConfigPrettier
+];
